c5fbe3be0ddb4b8addd292dac8311d450151af9d
Deivily <deivily.ramone@gmail.com>
2021-05-24 17:18:19 -0300

    commit inicial projeto cities-api

---
+++ Bootcamps/everis-qa-beginner/cities-distance-api/src/main/java/com/github/deivily/citiesdistanceapi/CitiesDistanceApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.deivily.citiesdistanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesDistanceApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CitiesDistanceApiApplication.class, args);
+	}
+
+}
---
+++ Bootcamps/everis-qa-beginner/cities-distance-api/src/test/java/com/github/deivily/citiesdistanceapi/CitiesDistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deivily.citiesdistanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesDistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
